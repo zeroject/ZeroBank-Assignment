ee3ef1001ca4bf0ccba75b11636ee4354b778aba
Zeroject <cypeyt@gmail.com>
2022-10-26 10:55:04 +0200

    fix fix

--- ZeroBank/TestProject1/UnitTest1.cs
+++ ZeroBank/TestProject1/UnitTest1.cs
@@ -12,14 +12,7 @@ namespace TestProject1
         {
             List<Product> products = new List<Product>() { new Product() { Id = 1, Name = "Mock", Price = 54 } };
 
-            Assert.Equal(1, products.Count);
-        }
-        [Fact]
-        public void Test2()
-        {
-            List<Product> products = new List<Product>() { new Product() { Id = 1, Name = "Mock", Price = 54 } };
-
-            Assert.Equal(repository.GetAllProducts(), products);
+            Assert.Equal(repository.GetAllProducts().Count, products.Count);
         }
     }
 }
